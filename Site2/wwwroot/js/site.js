b31acdd07eaf0b7e81e246c7236ec618707d40b3
Regber <regber.888@gmail.com>
2022-08-22 17:58:28 +0300

    Изменены стили и добавлен скрипт изменяющий размер центральной строки грида .buttons с тем что бы нижняя часть линии всегда находилась у нижнего края страницы.

--- Site2/wwwroot/js/site.js
+++ Site2/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var buttons = document.querySelector('.buttons');
+
+$(window).ready(function(){
+    SetCurveSize();
+});
+
+$(window).resize(function () {
+    SetCurveSize();
+});
+
+function SetCurveSize() {
+    buttons.style.setProperty('--central-row-height', $(window).height() - 525 + 'px');
+}
+
