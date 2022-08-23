e5ef8f86e13bcb76424fc6902a3f795e2c38ae4c
Regber <regber.888@gmail.com>
2022-08-23 13:14:31 +0300

    Закончен скрипт анимирующий движение мыши.

--- Site2/wwwroot/js/site.js
+++ Site2/wwwroot/js/site.js
@@ -3,16 +3,92 @@
 
 // Write your Javascript code.
 var buttons = document.querySelector('.buttons');
+var movementObject = document.querySelector('.movement');
+var mouseTopMoveLimit = 97;
+var mouseLowerMoveLimit = 0;
+var mouseCurrentPos = 97;
+var curveSize = 799;
+var intervalEvent = null;
 
 $(window).ready(function(){
     SetCurveSize();
+    SetMouseLowerMoveLimit()
 });
 
 $(window).resize(function () {
     SetCurveSize();
+    SetMouseLowerMoveLimit();
+});
+
+window.addEventListener("wheel", function (e) {
+    
+    if (e.wheelDelta > 0) {
+        //console.log("up");
+        if (mouseCurrentPos < mouseTopMoveLimit) {
+            MouseMove(mouseCurrentPos + 10);
+        }
+    }
+    else {
+        //console.log("down");
+        if (mouseCurrentPos > mouseLowerMoveLimit) {
+            MouseMove(mouseCurrentPos - 10);
+        }
+    }
 });
 
 function SetCurveSize() {
-    buttons.style.setProperty('--central-row-height', $(window).height() - 525 + 'px');
+
+    let windowHeight = $(window).height()
+
+    if (windowHeight < 765) {
+        buttons.style.setProperty('--central-row-height',  240 + 'px');
+    }
+    else {
+        buttons.style.setProperty('--central-row-height', windowHeight - 525 + 'px');
+    }
+}
+
+function SetMouseLowerMoveLimit() {
+
+    var buttonsStyles = window.getComputedStyle(buttons);
+    var centralRowHeight = buttonsStyles.getPropertyValue('--central-row-height');
+    
+    mouseLowerMoveLimit = (((curveSize - parseFloat(centralRowHeight)) / curveSize) * 100 - 13);
+    
+}
+
+function MouseMove(newPosition) {
+
+    clearInterval(intervalEvent);
+
+    intervalEvent = setInterval(frame, 20);
+
+    function frame() {
+        //Проверяем в какую сторону смещается мышь
+        if (newPosition >= mouseCurrentPos) {
+            mouseCurrentPos++;
+        }
+        else {
+            mouseCurrentPos--;
+        }
+
+        if (mouseLowerMoveLimit >= mouseCurrentPos || mouseCurrentPos >= mouseTopMoveLimit || newPosition == mouseCurrentPos) {
+            clearInterval(intervalEvent);
+            if (mouseLowerMoveLimit >= mouseCurrentPos) {
+                SetMousePosition(mouseLowerMoveLimit);
+            }
+            if (mouseCurrentPos >= mouseTopMoveLimit) {
+                SetMousePosition(mouseTopMoveLimit);
+
+            }
+        } else {
+            SetMousePosition(mouseCurrentPos);
+        }
+
+    }
+}
+
+function SetMousePosition(position){
+    movementObject.style.setProperty('offset-distance', position + '%');
 }
 
