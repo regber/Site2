421085e080816698463f92778f30c720ae70dfc3
Regber <regber.888@gmail.com>
2022-08-25 12:47:48 +0300

    Добавлена шестая страница.

---
+++ Site2/Pages/Page6.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Site2.Pages
+{
+    public class Page6Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
